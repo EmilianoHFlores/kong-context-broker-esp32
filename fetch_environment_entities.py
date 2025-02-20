6435da822eeb771e31a4446de102da893b8240cb
EmilianoHFlores <A00833160@tec.mx>
2025-02-20 00:26:17 -0600

    Fetch all type entities script

---
+++ fetch_environment_entities.py
@@ -0,0 +1,21 @@
+from OrionManager import OrionManager
+import os
+
+if __name__ == "__main__":
+    client_id = os.getenv("KEYCLOAK_CLIENT_ID")
+    client_secret = os.getenv("KEYCLOAK_CLIENT_SECRET")
+    keycloak_url = os.getenv("KEYCLOAK_URL")
+    orion_url = os.getenv("ORION_URL")
+
+    if not client_id or not client_secret:
+        print("Client ID and Client Secret must be provided in the .env file.")
+        exit(1)
+
+    manager = OrionManager(client_id, client_secret, keycloak_url, orion_url)
+    
+    entity_type = "ParticleSensor"
+    
+    entities = manager.fetch_entities(entity_type)
+    
+    print(entities)
+    print(f"Total entities: {len(entities)}")
